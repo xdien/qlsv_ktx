7e66fd6595cb105008df7c3511b52633ca01fa9f
Xuan Dien <bdienw@gmail.com>
2014-05-05 20:46:26 +0700

    Xoa cac file khong can

--- SCRIPT.sql
+++
@@ -1,9 +0,0 @@
-select 
-    *
-FROM
-    HOP_DONG
-        left join
-    CUA_PHONG ON HOP_DONG.ma_phong = CUA_PHONG.ma_phong
-        LEFT JOIN
-    TAI_SAN ON CUA_PHONG.ma_ts = TAI_SAN.ma_ts
-WHERE HOP_DONG.MSSV = 'ct1056m012' 
